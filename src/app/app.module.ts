8cb71e28327743f74b41f86680a6e7cef5ed502a
Anselme Guillaume <anselmeguillaume@orange.fr>
2023-04-20 16:11:33 +0200

    tour -> Guillaume

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodosModule } from './todos/todos/todos.module';
+import { TodosModule } from './todos/todos.module';
+import { MatToolbarModule } from '@angular/material/toolbar'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, TodosModule],
+  imports: [BrowserModule, AppRoutingModule, TodosModule, MatToolbarModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/todos/todos-material.module.ts
+++ src/app/todos/todos-material.module.ts
@@ -1,6 +1,19 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
-const angularMaterialModules: any[] = [];
+const angularMaterialModules: any[] = [
+  MatButtonModule,
+  MatCheckboxModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule
+];
 
 @NgModule({
   imports: [...angularMaterialModules],
